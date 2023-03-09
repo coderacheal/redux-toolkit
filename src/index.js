91f47e3de1c63f67e8629f819de979abd9a49ac9
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-03-09 13:10:43 +0000

    Add store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
+// import { ReactDOM } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-
+// const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
